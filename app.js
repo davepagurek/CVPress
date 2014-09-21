cfc33dc876b108cbfe40df7e5524a459af580638
Dave Pagurek <dave@davepagurek.com>
2014-09-21 04:45:58 -0400

    and we're back

--- app.js
+++ app.js
@@ -178,9 +178,9 @@ app.get('/api', apiController.getApi);
 app.get('/api/facebook', passportConf.isAuthenticated, passportConf.isAuthorized, apiController.getFacebook);
 app.get('/api/github', passportConf.isAuthenticated, passportConf.isAuthorized, apiController.getGithub);
 //app.get('/api/linkedin', passportConf.isAuthenticated, passportConf.isAuthorized, apiController.getLinkedin);
-/*app.get('/api/linkedin', passportConf.isAuthenticated, passportConf.isAuthorized, function(req, res){
+app.get('/json/linkedin', passportConf.isAuthenticated, passportConf.isAuthorized, function(req, res){
   res.redirect("/#linkedin");
-});*/
+});
 app.get('/api/linkedin', apiController.getLinkedin);
 
 /**
