c44029e8ad3b5b6f0621ff9c931db52b2c42912c
patrickpowns <patrickpowns@gmail.com>
2014-09-20 23:25:57 -0400

    getting 404 errors, but download works

--- controllers/html2pdf.js
+++ controllers/html2pdf.js
@@ -1,22 +1,17 @@
 var sys = require('sys')
 var exec = require('child_process').exec;
+var path = require('path');
 
-exports.render = function(req, res) {  
-//  console.log("k " + req.body.json2);
-  res.writeHead(200, {"Content-Type": "text/plain"});
-  res.end("k " + req.body.json2);
-
+exports.render = function(req, res) {
   
   function puts(error, stdout, stderr) { sys.puts(stdout) }
-  exec("wkhtmltopdf http://www.unixtimestamp.com/index.php resume.pdf", function (error, stdout, stderr) {
+  exec("wkhtmltopdf http://www.unixtimestamp.com/index.php public/resume_gen.pdf", function (error, stdout, stderr) {
     sys.print('stdout: ' + stdout);
     sys.print('stderr: ' + stderr);
     if (error != null) {
       console.log('execution error: ' + error);
     }
+    console.log("k " + req.body.json2 + "\n" + req.body.token);
+    res.sendFile(path.resolve("public/resume.pdf"));
   });
-  
-  res.render('privacy.html');
-//  res.sendFile("../resume.pdf");
 };
-
