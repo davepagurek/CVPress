e41392a375ce4015e314707cb94270a4c361d1cc
patrickpowns <patrickpowns@gmail.com>
2014-09-21 11:28:56 -0400

    okay it work html2pdf pls

--- controllers/html2pdf.js
+++ controllers/html2pdf.js
@@ -1,18 +1,29 @@
 var sys = require('sys')
 var exec = require('child_process').exec;
-var wkhtmltopdf = require('wkhtmltopdf');
+var path = require('path');
+var fs = require('fs');
 
 exports.render = function(req, res) {
   var wkhtmltopdf_path = process.env.PORT ? './bin/wkhtmltopdf-linux-amd64' : 'wkhtmltopdf';
   // req.body.json2 is the resume as HTML
   wkhtmltopdf(req.body.json2).pipe(res)
   
-  console.log('generating ' + pdf_path + ' out of ' + pdf_url)
-  child = exec([wkhtmltopdf_path, '--print-media-type', '--no-background', protocol + '://' + pdf_url, pdf_path].join(' '),
-  function (error, stdout, stderr) {
-    console.log('stdout: ' + stdout);
-    console.log('stderr: ' + stderr);
-    if (error !== null) {
-    console.log('exec error: ' + error);
-  }});
+  fs.writeFile(path.resolve("public/resume.html"), req.body.json2, function(err) {
+    if(err) {
+      console.log(err);
+    } else {
+      console.log("The file was saved!");
+    }
+  }); 
+  
+  function puts(error, stdout, stderr) { sys.puts(stdout) }
+  exec(wkhtmltopdf_path + " public/resume.html public/resume.pdf", function (error, stdout, stderr) {
+    sys.print('stdout: ' + stdout);
+    sys.print('stderr: ' + stderr);
+    if (error != null) {
+      console.log('execution error: ' + error);
+    }
+    console.log("k " + req.body.json2 + "\n" + req.body.token);
+    res.sendFile(path.resolve("public/resume.pdf"));
+  });
 };
