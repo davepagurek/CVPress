390ebe07a79a5fdd1ad02d02f4ffde572480d876
patrickpowns <patrickpowns@gmail.com>
2014-09-21 02:13:59 -0400

    wkhtmltopdftonode oh my god wheeee

--- controllers/html2pdf.js
+++ controllers/html2pdf.js
@@ -1,26 +1,8 @@
 var sys = require('sys')
 var exec = require('child_process').exec;
-var path = require('path');
-var fs = require('fs');
+var wkhtmltopdf = require('wkhtmltopdf');
 
 exports.render = function(req, res) {
   
-  fs.writeFile(path.resolve("public/resume.html"), req.body.json2, function(err) {
-    if(err) {
-        console.log(err);
-    } else {
-        console.log("The file was saved!");
-    }
-  }); 
-  
-  function puts(error, stdout, stderr) { sys.puts(stdout) }
-  exec("wkhtmltopdf public/resume.html public/resume.pdf", function (error, stdout, stderr) {
-    sys.print('stdout: ' + stdout);
-    sys.print('stderr: ' + stderr);
-    if (error != null) {
-      console.log('execution error: ' + error);
-    }
-    console.log("k " + req.body.json2 + "\n" + req.body.token);
-    res.sendFile(path.resolve("public/resume.pdf"));
-  });
+  wkhtmltopdf(req.body.json2).pipe(res)
 };
