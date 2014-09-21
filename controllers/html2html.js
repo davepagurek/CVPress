8f3d177ed58a01090975d1409e28d4269f9fb415
patrickpowns <patrickpowns@gmail.com>
2014-09-21 11:21:08 -0400

    solving app error on heroku htmltopdf

--- controllers/html2html.js
+++ controllers/html2html.js
@@ -1,4 +1,5 @@
 exports.render = function(req, res) {
+  console.log("writinghtml");
   res.writeHeader(200, {"Content-Type": "text/html"});  
   res.write(req.body.html);
   res.end(); 
--- controllers/html2pdf.js
+++ controllers/html2pdf.js
@@ -3,6 +3,16 @@ var exec = require('child_process').exec;
 var wkhtmltopdf = require('wkhtmltopdf');
 
 exports.render = function(req, res) {
+  var wkhtmltopdf_path = process.env.PORT ? './bin/wkhtmltopdf-linux-amd64' : 'wkhtmltopdf';
   // req.body.json2 is the resume as HTML
   wkhtmltopdf(req.body.json2).pipe(res)
+  
+  console.log('generating ' + pdf_path + ' out of ' + pdf_url)
+  child = exec([wkhtmltopdf_path, '--print-media-type', '--no-background', protocol + '://' + pdf_url, pdf_path].join(' '),
+  function (error, stdout, stderr) {
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    if (error !== null) {
+    console.log('exec error: ' + error);
+  }});
 };
