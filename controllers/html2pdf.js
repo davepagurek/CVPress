c2f84278e076d2d9b64fbdc278a44e04f0582c77
patrickpowns <patrickpowns@gmail.com>
2014-09-20 23:59:20 -0400

    once html strings are given to this, simple update and it'll work theoretically

--- controllers/html2pdf.js
+++ controllers/html2pdf.js
@@ -1,11 +1,20 @@
 var sys = require('sys')
 var exec = require('child_process').exec;
 var path = require('path');
+var fs = require('fs');
 
 exports.render = function(req, res) {
   
+  fs.writeFile(path.resolve("public/resume.html"), req.body.json2, function(err) {
+    if(err) {
+        console.log(err);
+    } else {
+        console.log("The file was saved!");
+    }
+  }); 
+  
   function puts(error, stdout, stderr) { sys.puts(stdout) }
-  exec("wkhtmltopdf http://www.unixtimestamp.com/index.php public/resume.pdf", function (error, stdout, stderr) {
+  exec("wkhtmltopdf public/resume.html public/resume.pdf", function (error, stdout, stderr) {
     sys.print('stdout: ' + stdout);
     sys.print('stderr: ' + stderr);
     if (error != null) {
